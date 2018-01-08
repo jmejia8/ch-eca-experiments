18a8d7ac0d56c0b1826b146c9b80402b3d2ef181
Jesús Mejía <jesusmejded@gmail.com>
2018-01-07 20:30:31 -0600

    Experiment design

--- main.jl
+++ main.jl
@@ -3,45 +3,62 @@ using CEC17
 
 const NFUNS = 28
 const NRUNS = 25
+const ε     = 0.0001
 
-function fitnessFunc(x, func_num = 1)
-    f, g, h = cec17_test_COP(x, func_num)
-
-    return f + abs(sum(g)) + abs(sum(h))
+function G(g)
+    i = g .<= 0.0
+    
+    gg = copy(g)
+    gg[i] = 0.0
+    return gg
 end
 
-function runn(func_num)
+function H(h)
+    hh = abs.(h) - ε
+
+    i = h .<= 0.0
+    hh[i] = 0.0
+    return hh
+end
+
+
+
+function runn(func_num, run_num)
     D = 10
     K = 7
 
+    f,g,h = cec17_test_COP(zeros(D), func_num)
+    D_g = length(g)
+    D_h = length(h)
 
-    approx, ff = eca(fitnessFunc, D;
-                                  N = 2K*D,
+    fitnessFunc(x) = cec17_test_COP(x, func_num)
+
+    approx = ecaConstrained(fitnessFunc, D, D_g, D_h;
                               η_max = 2.0,
-                             limits = [-100.0, 100],
-                         correctSol = false,
+                             limits = searchRange(func_num),
+                         correctSol = true,
                         showResults = false,
-                        termination = x-> abs(std(-1 + 1.0 ./x)) < 1e-10,
-                          max_evals = 10000D)
+                            saveLast= "tmp/output/gen_f$(func_num)_$(run_num).csv",
+                    saveConvergence = "tmp/output/converg_f$(func_num)_$(run_num).csv",
+                          max_evals = 20000D)
 
-    return ff
+    c = sum(G(approx.g) .> 0.0) + sum(H(approx.h) .> 0.0)
+    return approx.f, approx.νVal, c
 end
 
 function main()
     
-    nruns = 1
+    nruns = NRUNS
     
     for f = 1:NFUNS
-        errores = zeros(nruns)
         
-        for r = 1:nruns
-            err = runn(f)
-            @printf("run = %d \t fn = %d \t error = %e \n", r, f, err)
-            errores[r] = err
+        for r = 2:nruns
+            ff, v, c = runn(f, r)
+            @printf("run = %d \t fnum = %d \t %e \t ν = %e \t c =  %d\n", r, f, ff, v,c)
         end
         
-        println("mean = ",mean(errores), " std = ", std(errores))
-        println("====================================\n")
+        println("====================================")
+        # break
     end
 end
 
